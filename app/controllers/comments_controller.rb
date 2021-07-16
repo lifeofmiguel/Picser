5070ef415a682f466385d35a7d69e64479ebae47
Miguel Angel Morales <mmorales21500@live.com>
2021-07-15 23:22:10 -0400

    added more methods in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,5 +2,14 @@ class CommentsController < ApplicationController
 
     def create
         @post = Image.find(params[:image_id])
-        @comment = @image.comments.create(params[:comment].permit(:name))
+        @comment = @image.comments.create(params[:comment].permit(:name, :body))
+        redirect_to post_path(@image)
+    end
+
+    def destroy
+        @image = Image.find(params[:image_id])
+        @comment = @image.comments.find(params[:id])
+        @comment.destroy
+        redirect_to image_path(@image)
+    end
 end
